ec8ffe1dbef962692a0cf1bbd7648bf112a9ef6b
Seth Valdetero <valdetero@gmail.com>
2015-03-09 09:13:03 -0500

    Updated Versions

--- Xamarin-Studio/Tools/ChocolateyInstall.ps1
+++ Xamarin-Studio/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'xamarin.studio' 'msi' "/Passive /NoRestart /Log $env:temp\xamarin.log" 'http://download.xamarin.com/studio/Windows/XamarinStudio-5.5.2.0-0.msi' 
+Install-ChocolateyPackage 'xamarin.studio' 'msi' "/Passive /NoRestart /Log $env:temp\xamarin.log" 'http://download.xamarin.com/studio/Windows/XamarinStudio-5.7.2.7-0.msi' 
--- Xamarin-VisualStudio/Tools/ChocolateyInstall.ps1
+++ Xamarin-VisualStudio/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'xamarin.visualstudio' 'msi' "/Passive /NoRestart /Log $env:temp\xamarin.log" 'http://download.xamarin.com/XamarinforVisualStudio/Windows/Xamarin.VisualStudio_Setup-3.7.230.msi' 
+Install-ChocolateyPackage 'xamarin.visualstudio' 'msi' "/Passive /NoRestart /Log $env:temp\xamarin.log" 'http://download.xamarin.com/XamarinforVisualStudio/Windows/Xamarin.VisualStudio_3.9.347.msi' 
