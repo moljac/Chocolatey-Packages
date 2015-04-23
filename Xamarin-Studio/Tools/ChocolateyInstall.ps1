904e69deb5d4f1e38dc94c11299d2f3b7aa05f4c
Seth Valdetero <valdetero@gmail.com>
2015-04-23 15:28:14 -0500

    Updated version

--- Xamarin-Studio/Tools/ChocolateyInstall.ps1
+++ Xamarin-Studio/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'xamarin.studio' 'msi' "/Passive /NoRestart /Log $env:temp\xamarin.log" 'http://download.xamarin.com/studio/Windows/XamarinStudio-5.7.2.7-0.msi' 
+Install-ChocolateyPackage 'xamarin.studio' 'msi' "/Passive /NoRestart /Log $env:temp\xamarin.log" 'http://download.xamarin.com/studio/Windows/XamarinStudio-5.8.3.1-0.msi' 
