8ac3a058ba01b5d52f8928077cee4a210813f4d6
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-09-21 00:19:27 +0200

    removed 32b and 64b

--- products-additional/Xamarin-Android-Player-32b/Tools/ChocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$version = '0.6.5'
-
-$packageName = 'xamarin.android.player.64b' 
-$installerType = 'exe' 
-$url = 'http://xamarin-android-player.s3.amazonaws.com/player/win/0.6.5/XAP-Beta-15.43-v0.6.5-i386.exe' 
-$silentArgs = "/Passive /NoRestart /Log $env:temp\xamarin.android.player.632b.log" 
-$validExitCodes = @(0, 3010)
-
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
