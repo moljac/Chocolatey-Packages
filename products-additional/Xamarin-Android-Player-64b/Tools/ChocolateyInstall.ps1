2d936a282b198cb951758cec02e6c534bd4f401f
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-09-22 19:10:04 +0200

    XAP 64b

---
+++ products-additional/Xamarin-Android-Player-64b/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,18 @@
+$version = '0.6.5'
+
+$packageName = 'xamarin.android.player.64b' 
+$installerType = 'exe' 
+$url64 = 'http://xamarin-android-player.s3.amazonaws.com/player/win/0.6.5/XAP-Beta-15.43-v0.6.5-amd64.exe' 
+$checksum64 = '51E967C23506872A2AEE5D962662D9A937CE9D64B80420B647218BB5C08B911D' 
+$checksumtype64 = 'sha256' 
+$silentArgs = "/Passive /NoRestart /Log $env:temp\xamarin.android.player.32b.log" 
+$validExitCodes = @(0, 3010)
+
+Install-ChocolateyPackage `
+    $packageName `
+    $installerType `
+    $silentArgs `
+    -Url      $url64 `
+    -Checksum       $checksum64 `
+    -ChecksumType   $checksumtype64 `
+	-validExitCodes $validExitCodes
