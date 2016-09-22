1fb9586173214b1b22ca9445f662a52c0786182b
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-09-22 19:08:04 +0200

    Xamarin.Studio updated links + sha

--- Xamarin-Studio/Tools/ChocolateyInstall.ps1
+++ Xamarin-Studio/Tools/ChocolateyInstall.ps1
@@ -1,9 +1,18 @@
-$version = '6.1.0.5441'
+$version = '6.1.0.5441-0'
 
 $packageName = 'xamarin.studio' 
 $installerType = 'msi' 
-$url = 'http://bosstoragemirror.blob.core.windows.net/wrench/monodevelop-windows-cycle8/68/68292d1ab289911c815ddc715dd7cc29a9752f9f/XamarinStudio-' + $version + '.msi' 
+$url = 'http://download.xamarin.com/studio/Windows/XamarinStudio-' + $version + '.msi'
+$checksum = '76EDF6D89C9A2C4FBBCFC2D3048B40D6F77290C891F32A1BDF0C097F929AEBCB' 
+$checksumtype = 'sha256' 
 $silentArgs = "/Passive /NoRestart /Log $env:temp\xamarin.log" 
 $validExitCodes = @(0, 3010)
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
+Install-ChocolateyPackage `
+    $packageName `
+    $installerType `
+    $silentArgs `
+    -Url      $url `
+    -Checksum       $checksum `
+    -ChecksumType   $checksumtype `
+	-validExitCodes $validExitCodes
