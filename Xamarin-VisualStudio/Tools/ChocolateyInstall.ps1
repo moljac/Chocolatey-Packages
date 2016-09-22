0e9addf980e0221d25c641f764182c631fcfd530
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-09-22 19:08:38 +0200

    Xamarin-VisualStudio update urls + sha

--- Xamarin-VisualStudio/Tools/ChocolateyInstall.ps1
+++ Xamarin-VisualStudio/Tools/ChocolateyInstall.ps1
@@ -2,8 +2,17 @@ $version = '4.2.0.680'
 
 $packageName = 'xamarin.visualstudio' 
 $installerType = 'msi' 
-$url = 'https://bosstoragemirror.blob.core.windows.net/wrench/xvs-win-cycle8/c4/c4382f51543833deb16514ef8f0d57803e89ce6c/Xamarin.VisualStudio_' + $version + '.msi' 
+$url = 'http://download.xamarin.com/XamarinforVisualStudio/Windows/Xamarin.VisualStudio_' + $version + '.msi'
+$checksum = 'DFB238FFDB943E4C0D66E6AF80A6F608A0BE79E6D120930FC766F7DD0DC57B76' 
+$checksumtype = 'sha256' 
 $silentArgs = "/Passive /NoRestart /Log $env:temp\xamarin.log" 
 $validExitCodes = @(0, 3010)
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
+Install-ChocolateyPackage `
+    $packageName `
+    $installerType `
+    $silentArgs `
+    -Url      $url `
+    -Checksum       $checksum `
+    -ChecksumType   $checksumtype `
+	-validExitCodes $validExitCodes
