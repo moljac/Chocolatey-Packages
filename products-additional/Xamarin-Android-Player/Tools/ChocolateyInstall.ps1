60179feb12380d4c2fd1b8c0c74fc8540b3757e0
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-09-26 22:41:33 +0200

    XAP quiet option attempt

--- products-additional/Xamarin-Android-Player/Tools/ChocolateyInstall.ps1
+++ products-additional/Xamarin-Android-Player/Tools/ChocolateyInstall.ps1
@@ -8,7 +8,7 @@ $checksum32 = '90B40F0BE7B0314ADA95F43BF38C1F94D97084842F85A6A8F5BF9CF83E004001'
 $checksumtype32 = 'sha256' 
 $checksum64 = '51E967C23506872A2AEE5D962662D9A937CE9D64B80420B647218BB5C08B911D' 
 $checksumtype64 = 'sha256' 
-$silentArgs = "/Passive /NoRestart /Log $env:temp\xamarin.android.player.log" 
+$silentArgs = "/Passive /Quiet /NoRestart /Log $env:temp\xamarin.android.player.log" 
 $validExitCodes = @(0, 3010)
 
 Install-ChocolateyPackage `
