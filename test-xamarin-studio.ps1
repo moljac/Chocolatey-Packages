87e893f6f369c455f7e216922818c978f798b886
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-10-03 10:12:01 +0200

    Xamarin Studio test script

--- test-xamarin-studio.ps1
+++ test-xamarin-studio.ps1
@@ -9,12 +9,13 @@ choco uninstall -y `
 choco uninstall -y `
     microsoft-build-tools-2013
 
+<#
 choco install -y `
     microsoft-build-tools-2013 `
     -source .\products-additional\microsoft-build-tools-2013\microsoft-build-tools-2013.2013.1.nupkg
-
 choco install -y `
     gtksharp 
+#>	
 choco install -y `
     xamarin-studio `
     -source .\Xamarin-Studio\xamarin-studio.6.1.1.15.nupkg
