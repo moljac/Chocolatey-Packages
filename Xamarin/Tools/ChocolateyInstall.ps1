3e51057513bea2473e97aa05286e33c1918c4d53
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-10-13 16:41:06 +0200

    Xamarin AutoHotKeys installation

--- Xamarin/Tools/ChocolateyInstall.ps1
+++ Xamarin/Tools/ChocolateyInstall.ps1
@@ -1,24 +1,39 @@
+$ErrorActionPreference = 'Stop';
+ 
 $packageName = 'xamarin' 
+$toolsDir   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $installerType = 'exe' 
 $url = 'http://download.xamarin.com/Installer/Windows/XamarinInstaller.exe' 
 $checksum = 'EE6495607A42AA0E2676F9CA24149565E81BAE73F05C4567047ED23741798231' 
 $checksumtype = 'sha256' 
-#$silentArgs = "/Passive /NoRestart /Log $env:temp\xamarin.log" 
+$silentArgs = "/VERYSILENT /NORESTART /NOCANCEL /PASSIVE /Log $env:temp\xamarin.log" 
 $validExitCodes = @(0, 3010)
+ 
+#Thanks to dtgm and the GitHub package for ideas.
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $toolsDir "install-xamarin.ahk"
+$ahkProc = Start-Process `
+				-FilePath $ahkExe `
+                -ArgumentList $ahkFile `
+				-PassThru
+ 
+$ahkId = $ahkProc.Id
+Write-Debug "$ahkExe start time:`t$($ahkProc.StartTime.ToShortTimeString())"
+Write-Debug "Process ID:`t$ahkId"
  
 Install-ChocolateyPackage `
     $packageName `
     $installerType `
-	--verbose `
-	--debug `
-	--accept-license `
-	--confirm `
-	--not-silent `
+    $silentArgs `
     -Url      $url `
     -Checksum       $checksum `
     -ChecksumType   $checksumtype `
-	-validExitCodes $validExitCodes
+	-validExitCodes $validExitCodes `
 
-	
-#   $silentArgs `
+
+#    --not-silent `
+#   --verbose
+#	--debug `
+#    --accept-license `
+#    --confirm `
 	
