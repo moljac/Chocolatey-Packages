61176ba958598c5ab51b051e66bbc38ed11b2d2f
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-11-17 18:57:36 +0100

    interactive 1.0.0.0

--- products-additional/Xamarin-Interactive-Workbooks-Inspector/Tools/ChocolateyInstall.ps1
+++ products-additional/Xamarin-Interactive-Workbooks-Inspector/Tools/ChocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$version = '0.99.2.0'
+$version = '1.0.0.0'
 
 $packageName = 'xamarin.interactive.aka.workbooks.inspector' 
 $installerType = 'msi' 
 $url = 'https://dl.xamarin.com/interactive/XamarinInteractive-' + $version + '.msi' 
-$checksum32 = '3C0C84A69627EE3C71D716F5012ED9453591F91617EE8B15B0953EC3E8E84F4A' 
+$checksum32 = '3D18FF404995B731C4F0B94B97B44A7F604AE393D712EA81FC87477AB8CD975F' 
 $checksumtype32 = 'sha256'
 $silentArgs = "/Passive /NoRestart /Log $env:temp\xamarin.log" 
 $validExitCodes = @(0, 3010)
