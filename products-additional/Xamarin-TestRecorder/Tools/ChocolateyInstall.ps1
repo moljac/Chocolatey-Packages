2e920166a5ddec168865add4d08a3870e6bc68ec
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-01-04 17:47:21 +0100

    update test recorder

--- products-additional/Xamarin-TestRecorder/Tools/ChocolateyInstall.ps1
+++ products-additional/Xamarin-TestRecorder/Tools/ChocolateyInstall.ps1
@@ -2,8 +2,8 @@ $version = '0.4.2'
 
 $packageName = 'xamarin.testrecorder.vs2015' 
 $installerType = 'vsix' 
-$url = 'https://visualstudiogallery.msdn.microsoft.com/f9171f80-291e-480c-8003-8bf5c575f2dc/file/221714/5/Xamarin.TestRecorder.VSIX.2015.vsix'
-$checksum32 = '17CE46FB82E4EF548C4E0C90141A386B87165F0CCBBA8844EC4CED2D44365335' 
+$url = 'https://marketplace.visualstudio.com/items?itemName=XamarinInc.XamarinTestRecorder2015'
+$checksum32 = 'D1210E0DDB21DAB72A49A14F78D95943F15F4A3AB0C26F072E9E3C4EEE01827C' 
 $checksumtype32 = 'sha256'
 $silentArgs = "/Passive /NoRestart /Log $env:temp\xamarin.testrecorder.vs2015.log" 
 $validExitCodes = @(0, 3010)
